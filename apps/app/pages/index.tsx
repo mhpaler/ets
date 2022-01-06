becb11082080c013ce1f6d9bbdc7724988cc602f
swaylock <michael@ets.xyz>
2022-01-05 20:35:34 -0800

    fix unescaped apostrophe.
    gh-21

--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API. Let's use Gitbook instead.</p>
+            <p>{"Find in-depth information about Next.js features and API. Perhaps we'll use Gitbook for this though."}</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
