0c530243eb4aea1ec72b7ef632e9ff7fe5387559
Zach Williams <zawilliams@gmail.com>
2022-01-27 17:09:58 -0600

    Setup global SWR config

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
+import { fetcher } from '../utils/fetchers';
 import Layout from '../layouts/default';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: fetcher,
+      }}
+    >
       <Layout>
         <Component {...pageProps} />
       </Layout>
+    </SWRConfig>
   );
 }
