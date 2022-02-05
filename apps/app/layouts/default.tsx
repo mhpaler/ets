6499023571ae6d12e95cb67c3a526c4e8c98e17d
Zach Williams <zawilliams@gmail.com>
2022-02-04 20:35:16 -0600

    Style updates
    Removed an unneeded style and updated the Next wrapper div so we can use flexbox and have a sticky footer

--- apps/app/layouts/default.tsx
+++ apps/app/layouts/default.tsx
@@ -5,17 +5,17 @@ import Footer from '../components/Footer';
 
 export default function Layout({ children }: {children: ReactNode}) {
   return (
-    <div className="antialiased bg-white">
+    <>
       <Head>
         <title>Ethereum Tag Service</title>
         <meta name="description" content="Ethereum Tag Service is the community-owned incentivized cross-chain content tagging protocol for the decentralized web." />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </Head>
       <Navbar />
-      <main className="px-4">
+      <main className="flex-grow px-4">
         {children}
       </main>
       <Footer />
-    </div>
+    </>
   )
 }
