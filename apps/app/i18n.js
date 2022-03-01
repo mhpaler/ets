7c4c6edcd22cf2bf5c28d361663c87b38315607e
Zach Williams <zawilliams@gmail.com>
2022-02-28 18:08:15 -0600

    Added new Number component with localization

--- apps/app/i18n.js
+++ apps/app/i18n.js
@@ -1,13 +1,14 @@
-const formatters = {
 const buildFormatter = require('react-timeago/lib/formatters/buildFormatter').default;
 const timeagoEn = require('react-timeago/lib/language-strings/en').default;
 const timeagoEs = require('react-timeago/lib/language-strings/es').default;
 
+const numberFormatters = {
   en: new Intl.NumberFormat("en-EN"),
   es: new Intl.NumberFormat("es-ES"),
 };
 
 module.exports = {
+  numberFormatters: numberFormatters,
   timeagoFormatters: {
     'en': buildFormatter(timeagoEn),
     'es': buildFormatter(timeagoEs),
@@ -20,7 +21,7 @@ module.exports = {
   },
   interpolation: {
     format: (value, format, lang) => {
-      if (format === 'number') return formatters[lang].format(value);
+      if (format === 'number') return numberFormatters[lang].format(value);
       return value;
     }
   }
