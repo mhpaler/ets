2172e6f3e74e787fe68989ba38176c80fbcb56be
swaylock <michael@ets.xyz>
2022-02-28 16:52:28 -0800

    Add animation placeholder.
    gh-30

---
+++ apps/site/components/animation.tsx
@@ -0,0 +1,11 @@
+
+
+export default () => {
+  return (
+    <div className="mx-auto max-w-full w-[880px] text-center px-4 mb-10">
+      <p className="text-lg mb-2 text-gray-600 md:!text-2xl">
+        Animation gonna go here.
+      </p>
+      </div>
+  )
+}
