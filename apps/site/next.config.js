6b8a912c06fb11be159c33cf46a927a4ac392e3f
swaylock <michael@ets.xyz>
2022-03-08 17:50:40 -0800

    Add simple redirect to Explorer.
    gh-65

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -12,6 +12,16 @@ const withNextra = require('nextra')({
  **/
  const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/explorer',
+        destination: 'https://app-f0rqgtug1-ethereum-tag-service.vercel.app/',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
 };
 
 module.exports = withNextra(withTM(nextConfig));
