057abb9c332ab6bf489382109afb8a9d00da4b30
Zach Williams <zawilliams@gmail.com>
2022-03-17 15:42:18 -0500

    Undo trigger build

--- apps/api/pages/index.tsx
+++ apps/api/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-// Trigger a build
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
--- apps/site/pages/_app.js
+++ apps/site/pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.css';
 import 'nextra-theme-docs/style.css';
 
-// Trigger a build
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
