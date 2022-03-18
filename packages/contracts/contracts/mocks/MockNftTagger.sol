aff928eaa3ee8ae44bfcdf51422371e67b75ca17
Vincent <vince0656@googlemail.com>
2022-03-18 12:09:55 +0000

    initial pass at sponsoring tagging via tagger signature and signature module which can be added to any target type gh-16

--- packages/contracts/contracts/mocks/MockNftTagger.sol
+++ packages/contracts/contracts/mocks/MockNftTagger.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.12;
 
 import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import { UUPSUpgradeable } from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { StringsUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
+import { TargetTypeSignatureModule } from "../signature/TargetTypeSignatureModule.sol";
 import { IETSTargetType, IERC165 } from "../interfaces/IETSTargetType.sol";
 import { IETS } from "../interfaces/IETS.sol";
 
 // example implementation of 1 target type tagging subcontract
-contract MockNftTagger is IETSTargetType, OwnableUpgradeable, UUPSUpgradeable, PausableUpgradeable {
+contract MockNftTagger is IETSTargetType, TargetTypeSignatureModule, OwnableUpgradeable, UUPSUpgradeable, PausableUpgradeable {
 
     /// @notice Address that built the target type smart contract
     address payable public override creator;
@@ -31,6 +32,7 @@ contract MockNftTagger is IETSTargetType, OwnableUpgradeable, UUPSUpgradeable, P
         __Ownable_init();
         __UUPSUpgradeable_init();
         __Pausable_init();
+        __TargetTypeSignatureModule_init(name(), version());
 
         ets = _ets;
         creator = _creator;
@@ -49,26 +51,96 @@ contract MockNftTagger is IETSTargetType, OwnableUpgradeable, UUPSUpgradeable, P
         string calldata _nftAddress,
         string calldata _tokenId,
         string calldata _chainId,
+        string calldata _tagString,
         address payable _publisher,
         address _tagger,
-        string calldata _tagString,
         bool _ensure
     ) external payable {
-        // TODO - consider whether such a check would be useful
-        // require(_tagger != _publisher, "");
+        // compute concatenated target URI from tag params
+        string memory targetURI = computeTargetURI(
+            _nftAddress,
+            _tokenId,
+            _chainId
+        );
 
-        // Extra layers that could be added: if EVM is from the same chain, validation can be performed
-        // targetURI boilerplate format for EVM Nft is contract address|token id|chain id
-        // eg "0x8ee9a60cb5c0e7db414031856cb9e0f1f05988d1|3061|1"
-        string memory targetURI = string(abi.encodePacked(_nftAddress, "|", _tokenId, "|", _chainId));
+        // call ETS informing of a new tag
+        _tag(
+            targetURI,
+            _publisher,
+            _tagString,
+            _tagger,
+            _ensure
+        );
+    }
+
+    /// @notice where tagger does an off chain signature and the tx is sponsored
+    function sponsoredTag(
+        string calldata _nftAddress,
+        string calldata _tokenId,
+        string calldata _chainId,
+        string calldata _tagString,
+        address payable _publisher,
+        uint8 _taggerV,
+        bytes32 _taggerR,
+        bytes32 _taggerS,
+        bool _ensure
+    ) external payable {
+        // compute concatenated target URI from tag params
+        string memory targetURI = computeTargetURI(
+            _nftAddress,
+            _tokenId,
+            _chainId
+        );
+
+        // recover tagger address from signature
+        address tagger = recoverAddress(targetURI, _taggerV, _taggerR, _taggerS);
+
+        // call ETS informing of a new tag
+        _tag(
+            targetURI,
+            _publisher,
+            _tagString,
+            tagger,
+            _ensure
+        );
+    }
+
+    function _tag(
+        string memory _targetURI,
+        address payable _publisher,
+        string calldata _tagString,
+        address _tagger,
+        bool _ensure
+    ) internal {
+        _performTaggingChecks(_tagger, _publisher);
 
         ets.tagTarget{ value: msg.value }(
             _tagString,
-            targetURI,
+            _targetURI,
             _publisher,
             _tagger,
             _ensure
         );
+    }
+
+    // Based on tagging params, concatenate the string in an ensure-compliant way
+    function computeTargetURI(
+        string calldata _nftAddress,
+        string calldata _tokenId,
+        string calldata _chainId
+    ) public pure returns (string memory) {
+        // Extra layers that could be added: if EVM is from the same chain, validation can be performed
+        // targetURI boilerplate format for EVM Nft is contract address|token id|chain id
+        // eg "0x8ee9a60cb5c0e7db414031856cb9e0f1f05988d1|3061|1"
+        return string(abi.encodePacked(_nftAddress, "|", _tokenId, "|", _chainId));
+    }
+
+    // Perform any checks on input data from the tagging event
+    function _performTaggingChecks(
+        address _tagger,
+        address _publisher
+    ) internal pure {
+        require(_tagger != _publisher, "Tagger cannot be publisher");
     }
 
     /// @notice Allow owner to toggle pausing on and off
@@ -89,11 +161,11 @@ contract MockNftTagger is IETSTargetType, OwnableUpgradeable, UUPSUpgradeable, P
 
     /// @inheritdoc IETSTargetType
     function name() public override pure returns (string memory) {
-        return "NFT";
+        return "MockNftTagger";
     }
 
     /// @inheritdoc IETSTargetType
-    function version() external override pure returns (string memory) {
+    function version() public override pure returns (string memory) {
         return "0.0.1";
     }
 
---
+++ packages/contracts/contracts/signature/TargetTypeSignatureModule.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.12;
+
+import { EIP712Upgradeable } from "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
+import { ECDSAUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+abstract contract TargetTypeSignatureModule is Initializable, EIP712Upgradeable {
+
+    bytes32 private _TAG_TYPEHASH;
+
+    function __TargetTypeSignatureModule_init(
+        string memory _contractName,
+        string memory _version
+    ) internal initializer {
+        __EIP712_init_unchained(_contractName, _version);
+
+        // tagParamsHash will be keccak256 of targetURI
+        _TAG_TYPEHASH = keccak256("Tag(bytes32 tagParamsHash)");
+    }
+
+    function recoverAddress(
+        string memory _targetURI,
+        uint8 _v,
+        bytes32 _r,
+        bytes32 _s
+    ) internal returns (address) {
+        bytes32 tagStructHash = keccak256(
+            abi.encode(_TAG_TYPEHASH, keccak256(bytes(_targetURI)))
+        );
+
+        return ECDSAUpgradeable.recover(
+            _hashTypedDataV4(tagStructHash),
+            _v,
+            _r,
+            _s
+        );
+    }
+}
