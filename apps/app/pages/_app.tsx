3a04587024c7b39d3bf302ad798ec433430c1142
Zach Williams <zawilliams@gmail.com>
2022-03-23 18:57:11 -0500

    Add nprogress for page loading indicator

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -3,6 +3,12 @@ import type { AppProps } from 'next/app';
 import { SWRConfig } from 'swr';
 import { fetcher } from '../utils/fetchers';
 import Layout from '../layouts/default';
+import nProgress from 'nprogress';
+import { Router } from 'next/router';
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
