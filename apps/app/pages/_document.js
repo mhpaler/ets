80b399e3cf6aebdabf1f4b1fe3a4a913ef059cd4
Zach Williams <zawilliams@gmail.com>
2022-04-18 16:32:35 -0500

    Added new classNames component

--- apps/app/pages/_document.js
+++ apps/app/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { classNames } from '../utils/classNames';
 
 export default class MyDocument extends Document {
   render() {
@@ -6,10 +7,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <body className={
+        <body className={classNames(
+          'selection:bg-sky-200 selection:text-sky-700 font-inter',
           pageProps.isDark ? 'dark-mode' : 'light-mode',
-          'selection:bg-sky-200 selection:text-sky-700 font-inter'
-        }>
+        )}>
           <Main />
           <NextScript />
         </body>
