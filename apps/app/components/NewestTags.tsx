3294d22414bc7f09ab75740779c4ac6d9081aaf0
Zach Williams <zawilliams@gmail.com>
2022-04-18 16:26:38 -0500

    Style updates to match new table styling

--- apps/app/components/NewestTags.tsx
+++ apps/app/components/NewestTags.tsx
@@ -10,22 +10,24 @@ const NewestTags = () => {
 
   return (
     <div className="w-full mx-auto">
-      <div className="rounded-md shadow-lg shadow-slate-400/20">
-        <Link href="/tags">
-          <a className="flex justify-between border border-b-0 rounded-t-md border-slate-200">
-            <div>
-              <h2 className="px-6 py-3 font-semibold text-left text-slate-700">{t('newest-tags')}</h2>
-            </div>
-            <div className="flex items-center pr-2">
-              <svg className="inline-flex w-6 h-6 text-pink-600 hover:text-pink-700" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.25 15.25V6.75H8.75"></path>
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 7L6.75 17.25"></path>
-              </svg>
-            </div>
-          </a>
-        </Link>
+      <div className="rounded-md shadow-lg shadow-slate-400/20 ring-1 ring-slate-200">
+        <div className="border-b border-slate-200">
+          <Link href="/tags">
+            <a className="flex justify-between rounded-t-md">
+              <div>
+                <h2 className="px-6 py-3 font-semibold text-left text-slate-700">{t('newest-tags')}</h2>
+              </div>
+              <div className="flex items-center pr-2">
+                <svg className="inline-flex w-6 h-6 text-pink-600 hover:text-pink-700" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.25 15.25V6.75H8.75"></path>
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 7L6.75 17.25"></path>
+                </svg>
+              </div>
+            </a>
+          </Link>
+        </div>
 
-        <div className="border divide-y border-slate-200 rounded-b-md divide-slate-200">
+        <div className="divide-y rounded-b-md divide-slate-200">
           {/* TODO: update :any to use type */}
           {tags && tags.map((tag: any) => (
             <div className="px-6 py-4 md:grid-flow-col md:space-x-4 md:grid md:grid-cols-2" key={tag.id}>
@@ -38,7 +40,7 @@ const NewestTags = () => {
                 <div className="text-sm leading-6 text-slate-500"><TimeAgo date={tag.timestamp * 1000} /></div>
               </div>
               <div className="grid grid-cols-2 space-x-4 md:block md:space-x-0">
-                <div className="flex space-x-2">
+                <div className="flex">
                   <span className="mr-1 text-slate-500">{t('creator')}</span>
                   <div className="flex-grow overflow-hidden text-right text-pink-600 hover:text-pink-700 text-ellipsis whitespace-nowrap">
                     <Link href={`/creators/${tag.creator}`}>
@@ -47,7 +49,7 @@ const NewestTags = () => {
                   </div>
                   <CopyAndPaste value={tag.creator} />
                 </div>
-                <div className="flex space-x-2">
+                <div className="flex">
                   <span className="mr-1 text-slate-500">{t('publisher')}</span>
                   <div className="flex-grow overflow-hidden text-right text-pink-600 hover:text-pink-700 text-ellipsis whitespace-nowrap">
                     <Link href={`/publishers/${tag.publisher}`}>
--- apps/app/components/TopCreators.tsx
+++ apps/app/components/TopCreators.tsx
@@ -10,22 +10,24 @@ const TopCreators = () => {
 
   return (
     <div className="w-full mx-auto">
-      <div className="rounded-md shadow-lg shadow-slate-400/20">
-        <Link href="/">
-          <a className="flex justify-between border border-b-0 rounded-t-md border-slate-200">
-            <div>
-              <h2 className="px-6 py-3 font-semibold text-left text-slate-700">{t('top-creators')}</h2>
-            </div>
-            <div className="flex items-center pr-2">
-              <svg className="inline-flex w-6 h-6 text-pink-600 hover:text-pink-700" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.25 15.25V6.75H8.75"></path>
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 7L6.75 17.25"></path>
-              </svg>
-            </div>
-          </a>
-        </Link>
+      <div className="rounded-md shadow-lg shadow-slate-400/20 ring-1 ring-slate-200">
+        <div className="border-b border-slate-200">
+          <Link href="/creators">
+            <a className="flex justify-between rounded-t-md">
+              <div>
+                <h2 className="px-6 py-3 font-semibold text-left text-slate-700">{t('top-creators')}</h2>
+              </div>
+              <div className="flex items-center pr-2">
+                <svg className="inline-flex w-6 h-6 text-pink-600 hover:text-pink-700" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17.25 15.25V6.75H8.75"></path>
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M17 7L6.75 17.25"></path>
+                </svg>
+              </div>
+            </a>
+          </Link>
+        </div>
 
-        <div className="border divide-y border-slate-200 rounded-b-md divide-slate-200">
+        <div className="divide-y rounded-b-md divide-slate-200">
           {/* TODO: update :any to use type */}
           {creators && creators.map((creator: any) => (
             <div className="grid grid-flow-col grid-cols-2 px-6 py-4 space-x-4" key={creator.id}>
