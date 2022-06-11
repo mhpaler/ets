e1d59621a1e1830e207c8aa2281252f34c5dd8b8
Zach Williams <zawilliams@gmail.com>
2022-06-11 17:25:08 -0500

    Added more chains for testing

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -21,7 +21,7 @@ import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-  [chain.polygonMumbai],
+  [chain.polygonMumbai, chain.hardhat, chain.localhost],
   [
     alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
     publicProvider()
