dad8644e201a30ed538a29565cc5e7336f583e7e
Zach Williams <zawilliams@gmail.com>
2022-06-11 17:45:47 -0500

    Rename Navbar default export

--- apps/app/components/Navbar.tsx
+++ apps/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import useTranslation from 'next-translate/useTranslation';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
-export default function Example() {
+export default function Navbar() {
   const { t } = useTranslation('common');
 
   return (
