3ed8af0f0475b5eeabd06ac85e0b376d3e4714c2
Zach Williams <zawilliams@gmail.com>
2022-06-11 16:06:46 -0500

    Added rainbowkit

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -5,6 +5,39 @@ import { fetcher } from '../utils/fetchers';
 import Layout from '../layouts/default';
 import nProgress from 'nprogress';
 import { Router } from 'next/router';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  lightTheme,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [chain.polygonMumbai],
+  [
+    alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Ethereum Tag Service',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
@@ -12,15 +45,26 @@ Router.events.on("routeChangeComplete", nProgress.done);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        refreshInterval: 3000,
-        fetcher: fetcher,
-      }}
-    >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SWRConfig>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={lightTheme({
+          accentColor: '#ec4899',
+          accentColorForeground: 'white',
+          borderRadius: 'medium',
+          overlayBlur: 'small',
+        })}>
+        <SWRConfig
+          value={{
+            refreshInterval: 3000,
+            fetcher: fetcher,
+          }}
+        >
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SWRConfig>
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
