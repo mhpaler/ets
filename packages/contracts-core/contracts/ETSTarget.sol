ec0ed85703bece60496ad25a6304c92dcf5fb25f
swaylock <michael@ets.xyz>
2022-07-25 11:40:50 -0400

    Document IETSTarget.sol
    gh-171

--- packages/contracts-core/contracts/ETSTarget.sol
+++ packages/contracts-core/contracts/ETSTarget.sol
@@ -11,6 +11,28 @@ import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 
 import "hardhat/console.sol";
 
+/**
+ * @title IETSTarget
+ * @author Ethereum Tag Service <team@ets.xyz>
+ *
+ * @notice This is the standard interface for the core ETSTarget.sol contract. It includes both public
+ * and administration functions.
+ *
+ * In ETS, a "Target" is our data structure, stored onchain, that references/points to a URI.
+ *
+ * For context, from Wikipedia, URI is short for Uniform Resource Identifier and is a unique sequence of
+ * characters that identifies a logical or physical resource used by web technologies. URIs may be used to
+ * identify anything, including real-world objects, such as people and places, concepts, or information
+ * resources such as web pages and books.
+ *
+ * For our purposes, as much as possible, we are restricting our interpretation of URIs to the more technical
+ * parameters defined by the IETF in [RFC3986](https://www.rfc-editor.org/rfc/rfc3986). For newer protocols, such
+ * as blockchains, For newer protocols, such as blockchains we will lean on newer emerging URI standards such
+ * as the [Blink](https://w3c-ccg.github.io/blockchain-links) and [BIP-122](https://github.com/bitcoin/bips/blob/master/bip-0122.mediawiki)
+ *
+ * One the thing to keep in mind with URIs & ETS Targets is that differently shaped URIs can sometimes point to the same
+ * resource. The effect of that is that different Target IDs in ETS can similarly point to the same resource.
+ */
 contract ETSTarget is IETSTarget, UUPSUpgradeable, StringHelpers {
     using AddressUpgradeable for address;
     using SafeMathUpgradeable for uint256;
@@ -19,14 +41,14 @@ contract ETSTarget is IETSTarget, UUPSUpgradeable, StringHelpers {
 
     IETSEnrichTarget public etsEnrichTarget;
 
-    /// Public constants
+    // Public constants
 
     string public constant NAME = "ETSTarget";
 
     /// @dev Map of targetId to Target struct.
     mapping(uint256 => Target) public targets;
 
-    /// Modifiers
+    // Modifiers
 
     modifier onlyAdmin() {
         require(etsAccessControls.isAdmin(msg.sender), "Caller must have administrator access");
@@ -43,12 +65,14 @@ contract ETSTarget is IETSTarget, UUPSUpgradeable, StringHelpers {
 
     // ============ OWNER INTERFACE ============
 
+    /// @inheritdoc IETSTarget
     function setAccessControls(address _etsAccessControls) public onlyAdmin {
         require(address(_etsAccessControls) != address(0), "Access controls cannot be zero");
         etsAccessControls = IETSAccessControls(_etsAccessControls);
         emit AccessControlsSet(address(_etsAccessControls));
     }
 
+    /// @inheritdoc IETSTarget
     function setEnrichTarget(address _etsEnrichTarget) public onlyAdmin {
         require(address(_etsEnrichTarget) != address(0), "ETSEnrichTarget address cannot be zero");
         etsEnrichTarget = IETSEnrichTarget(_etsEnrichTarget);
@@ -57,7 +81,7 @@ contract ETSTarget is IETSTarget, UUPSUpgradeable, StringHelpers {
 
     // ============ PUBLIC INTERFACE ============
 
-    /// @notice Get a targetId from target type and target URI.
+    /// @inheritdoc IETSTarget
     function getOrCreateTargetId(string memory _targetURI) public returns (uint256) {
         uint256 _targetId = computeTargetId(_targetURI);
         if (targets[_targetId].created != 0) {
@@ -67,6 +91,7 @@ contract ETSTarget is IETSTarget, UUPSUpgradeable, StringHelpers {
         return createTarget(_targetURI);
     }
 
+    /// @inheritdoc IETSTarget
     function createTarget(string memory _targetURI) public returns (uint256 targetId) {
         require(!targetExists(_targetURI), "target id exists");
 
@@ -76,26 +101,25 @@ contract ETSTarget is IETSTarget, UUPSUpgradeable, StringHelpers {
             createdBy: msg.sender,
             created: block.timestamp,
             enriched: 0,
-            httpstatus: 0,
+            httpStatus: 0,
             ipfsHash: ""
         });
         emit TargetCreated(_targetId);
         return _targetId;
     }
 
-    /// @notice Updates a target with new values.
-    /// @return success boolean
+    /// @inheritdoc IETSTarget
     function updateTarget(
         uint256 _targetId,
         string calldata _targetURI,
         uint256 _enriched,
-        uint256 _httpstatus,
+        uint256 _httpStatus,
         string calldata _ipfsHash
     ) external returns (bool success) {
         require(msg.sender == address(etsEnrichTarget), "Only ETSEnrichTarget may update target");
         targets[_targetId].targetURI = _targetURI;
         targets[_targetId].enriched = _enriched;
-        targets[_targetId].httpstatus = _httpstatus;
+        targets[_targetId].httpStatus = _httpStatus;
         targets[_targetId].ipfsHash = _ipfsHash;
 
         emit TargetUpdated(_targetId);
@@ -104,26 +128,31 @@ contract ETSTarget is IETSTarget, UUPSUpgradeable, StringHelpers {
 
     // ============ PUBLIC VIEW FUNCTIONS ============
 
+    /// @inheritdoc IETSTarget
     function computeTargetId(string memory _targetURI) public pure returns (uint256) {
         // ? Should we lowercase
         bytes32 targetId = keccak256(bytes(_targetURI));
         return uint256(targetId);
     }
 
+    /// @inheritdoc IETSTarget
     function targetExists(string memory _targetURI) public view returns (bool) {
         uint256 targetId = computeTargetId(_targetURI);
         return targetExists(targetId);
     }
 
+    /// @inheritdoc IETSTarget
     function targetExists(uint256 _targetId) public view returns (bool) {
         return targets[_targetId].created > 0 ? true : false;
     }
 
+    /// @inheritdoc IETSTarget
     function getTarget(string memory _targetURI) public view returns (Target memory) {
         uint256 targetId = computeTargetId(_targetURI);
         return getTarget(targetId);
     }
 
+    /// @inheritdoc IETSTarget
     function getTarget(uint256 _targetId) public view returns (Target memory) {
         return targets[_targetId];
     }
--- packages/contracts-core/contracts/interfaces/IETSTarget.sol
+++ packages/contracts-core/contracts/interfaces/IETSTarget.sol
@@ -3,87 +3,174 @@ pragma solidity ^0.8.0;
 
 /**
  * @title IETSTarget
- * @author Ethereum Tag Service
+ * @author Ethereum Tag Service <team@ets.xyz>
  *
- * @notice This is the standard interface for ETS Targets. It includes both public and administration functions.
+ * @notice This is the standard interface for the core ETSTarget.sol contract. It includes both public
+ * and administration functions.
+ *
+ * In ETS, a "Target" is our data structure, stored onchain, that references/points to a URI.
+ *
+ * For context, from Wikipedia, URI is short for Uniform Resource Identifier and is a unique sequence of
+ * characters that identifies a logical or physical resource used by web technologies. URIs may be used to
+ * identify anything, including real-world objects, such as people and places, concepts, or information
+ * resources such as web pages and books.
+ *
+ * For our purposes, as much as possible, we are restricting our interpretation of URIs to the more technical
+ * parameters defined by the IETF in [RFC3986](https://www.rfc-editor.org/rfc/rfc3986). For newer protocols, such
+ * as blockchains, For newer protocols, such as blockchains we will lean on newer emerging URI standards such
+ * as the [Blink](https://w3c-ccg.github.io/blockchain-links) and [BIP-122](https://github.com/bitcoin/bips/blob/master/bip-0122.mediawiki)
+ *
+ * One the thing to keep in mind with URIs & ETS Targets is that differently shaped URIs can sometimes point to the same
+ * resource. The effect of that is that different Target IDs in ETS can similarly point to the same resource.
  */
 interface IETSTarget {
     /**
-     * @notice Data structure for an ETS Target. The core utility of ETS is to record connections between CTAGs
-     * (our NFT token that represents a tag) and Targets. In ETS, a "Target" is our data structure, stored onchain,
-     * that references/points to a URI.
+     * @notice Data structure for an ETS Target.
      *
-     * For context, from Wikipedia, URI is short for Uniform Resource Identifier and is a unique sequence of
-     * characters that identifies a logical or physical resource used by web technologies. URIs may be used to
-     * identify anything, including real-world objects, such as people and places, concepts, or information
-     * resources such as web pages and books.
-     *
-     * For our purposes, as much as possible, we are restricting our interpretation of URIs to the more technical
-     * parameters defined by the IETF in RFC3986 (https://www.rfc-editor.org/rfc/rfc3986). For newer protocols, such
-     * as blockchains, other newer standards such a standards
-     *
-     * The thing to keep in mind with Targets & URIs is that differently shaped URIs can sometimes point to the same
-     * resource. The effect of that is that different Target IDs in ETS can similarly point to the same resource.
-     *
-     * @param targetURI Unique resource identifier for a target
-     * @param createdBy Address of TargetTagger that created target
-     * @param created block timestamp when target was created in ETS
-     * @param enriched block timestamp when target was last enriched. Defaults to 0
-     * @param httpstatus https status of last response from ensure enrich api eg. "404", "200". defaults to 0
-     * @param ipfsHash ipfsHash of additional metadata for target collected by ETS Enrich target API
+     * @param targetURI Unique resource identifier Target points to
+     * @param createdBy Address of IETSTargetTagger implementation that created Target
+     * @param created block timestamp when Target was created in ETS
+     * @param enriched block timestamp when Target was last enriched. Defaults to 0
+     * @param httpStatus https status of last response from ETSEnrichTarget API eg. "404", "200". defaults to 0
+     * @param ipfsHash ipfsHash of additional metadata for Target collected by ETSEnrichTarget API
      */
     struct Target {
         string targetURI;
         address createdBy;
         uint256 created;
         uint256 enriched;
-        uint256 httpstatus;
+        uint256 httpStatus;
         string ipfsHash;
     }
 
+    /**
+     * @dev emitted when the ETSAccessControls is set.
+     *
+     * @param etsAccessControls contract address ETSAccessControls is set to.
+     */
     event AccessControlsSet(address etsAccessControls);
 
+    /**
+     * @dev emitted when the ETSEnrichTarget API address is set.
+     *
+     * @param etsEnrichTarget contract address ETSEnrichTarget is set to.
+     */
     event EnrichTargetSet(address etsEnrichTarget);
 
+    /**
+     * @dev emitted when a new Target is created.
+     *
+     * @param targetId Unique Id of new Target.
+     */
     event TargetCreated(uint256 targetId);
 
+    /**
+     * @dev emitted when an existing Target is updated.
+     *
+     * @param targetId Id of Target being updated.
+     */
     event TargetUpdated(uint256 targetId);
 
     /**
-     * @notice Sets ETS access controls on the ETSTarget contract so that
-     * functions can be restricted to being called by ETS platform only.
+     * @notice Sets ETSAccessControls on the ETSTarget contract so functions can be
+     * restricted to ETS platform only.
      *
      * @param _etsAccessControls Address of ETSAccessControls contract.
      */
     function setAccessControls(address _etsAccessControls) external;
 
     /**
-     * @notice Sets ETS Enrich target contract address so that target metadata enrichment
+     * @notice Sets ETSEnrichTarget contract address so that Target metadata enrichment
      * functions can be called from ETSTarget.
      *
      * @param _etsEnrichTarget Address of ETSEnrichTarget contract.
      */
     function setEnrichTarget(address _etsEnrichTarget) external;
 
+    /**
+     * @notice Get ETS targetId from URI.
+     *
+     * Combo function that given a URI streing will return it's ETS targetId if it exists,
+     * or create a new Target record and return corresponding targetId.
+     *
+     * @param _targetURI URI passed in as string
+     * @return Id of ETS Target record
+     */
     function getOrCreateTargetId(string memory _targetURI) external returns (uint256);
 
+    /**
+     * @notice Create a Target record and return it's targetId.
+     *
+     * @param _targetURI URI passed in as string
+     * @return targetId Id of ETS Target record
+     */
     function createTarget(string memory _targetURI) external returns (uint256 targetId);
 
+    /**
+     * @notice Update a Target record.
+     *
+     * @param _targetId Id of Target being updated.
+     * @param _targetURI Unique resource identifier Target points to.
+     * @param _enriched block timestamp when Target was last enriched
+     * @param _httpStatus https status of last response from ETSEnrichTarget API eg. "404", "200". defaults to 0
+     * @param _ipfsHash ipfsHash of additional metadata for Target collected by ETSEnrichTarget API
+
+     * @return success true when Target is successfully updated.
+     */
     function updateTarget(
         uint256 _targetId,
         string calldata _targetURI,
         uint256 _enriched,
-        uint256 _httpstatus,
+        uint256 _httpStatus,
         string calldata _ipfsHash
     ) external returns (bool success);
 
+    /**
+     * @notice Function to deterministically compute & return a targetId.
+     *
+     * Every Target in ETS is mapped to by it's targetId. This Id is computed from
+     * the target URI sting hashed and cast as a uint256.
+     *
+     * Note: Function does not verify if Target record exists.
+     *
+     * @param _targetURI Unique resource identifier Target record points to.
+     * @return targetId Id of the potential Target record.
+     */
     function computeTargetId(string memory _targetURI) external view returns (uint256 targetId);
 
+    /**
+     * @notice Check that a Target record exists for a given URI string.
+     *
+     * @param _targetURI Unique resource identifier Target record points to.
+     * @return true if Target record exists; false if not.
+     */
     function targetExists(string memory _targetURI) external view returns (bool);
 
+    /**
+     * @notice Check that a Target record exists for a given computed targetId.
+     *
+     * @param _targetId targetId uint computed from URI via computeTargetId().
+     * @return true if Target record exists; false if not.
+     */
     function targetExists(uint256 _targetId) external view returns (bool);
 
+    /**
+     * @notice Retrieve a Target record for a given URI string.
+     *
+     * Note: returns a struct with empty members when no Target exists.
+     *
+     * @param _targetURI Unique resource identifier Target record points to.
+     * @return Target record.
+     */
     function getTarget(string memory _targetURI) external view returns (Target memory);
 
+    /**
+     * @notice Retrieve a Target record for a computed targetId.
+     *
+     * Note: returns a struct with empty members when no Target exists.
+     *
+     * @param _targetId targetId uint computed from URI via computeTargetId().
+     * @return Target record.
+     */
     function getTarget(uint256 _targetId) external view returns (Target memory);
 }
