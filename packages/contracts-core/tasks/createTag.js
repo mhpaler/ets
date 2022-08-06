f875910702e9eae9af8469690faf8b988c5f84c7
swaylock <michael@ets.xyz>
2022-08-06 09:29:35 -0700

    Polish up tasks.
    gh-178

--- packages/contracts-core/tasks/createTag.js
+++ packages/contracts-core/tasks/createTag.js
@@ -1,16 +1,35 @@
 task("createTag", "Create a tag")
   .addParam("tag", "Hashtag string")
   .setAction(async (taskArgs) => {
-    const {setup} = require("../test/setup.js");
+    const {getAccounts} = require("../test/setup.js");
     const config = require("../config/config.json");
-    const ETSTokenAbi = require("../abi/contracts/ETSToken.sol/ETSToken.json");
-    [accounts, contracts, initSettings] = await setup();
-    const ETSTokenAddress = config[31337].contracts.ETSToken.address;
-    const etsToken = new ethers.Contract(ETSTokenAddress, ETSTokenAbi, accounts.ETSPlatform);
+    const ETSTokenABI = require("../abi/contracts/ETSToken.sol/ETSToken.json");
+    const ETSAccessControlsABI = require("../abi/contracts/ETSAccessControls.sol/ETSAccessControls.json");
+    const chainId = hre.network.config.chainId;
+    const accounts = await getAccounts();
 
-    await etsToken.createTag(taskArgs.tag, accounts.ETSPlatform.address);
+    // Check that caller is publisher.
+    const ETSAccessControlsAddress = config[chainId].contracts.ETSAccessControls.address;
+    const etsAccessControls = new ethers.Contract(ETSAccessControlsAddress, ETSAccessControlsABI, accounts.ETSPlatform);
+    if ((await etsAccessControls.isPublisher(accounts.ETSPlatform.address)) === false) {
+      console.log("caller not a publisher");
+      return;
+    }
+
+    const ETSTokenAddress = config[chainId].contracts.ETSToken.address;
+    const etsToken = new ethers.Contract(ETSTokenAddress, ETSTokenABI, accounts.ETSPlatform);
+
+    console.log(`Minting "${taskArgs.tag}" at ${ETSTokenAddress}`);
     const tagId = await etsToken.computeTagId(taskArgs.tag);
+
     if (await etsToken["tagExists(uint256)"](tagId)) {
-      console.log("New tag created with id: ", tagId.toString());
+      console.log("Tag already exists with ID: ", tagId.toString());
+    } else {
+      const tx = await etsToken.connect(accounts.Buyer).createTag(taskArgs.tag, accounts.ETSPlatform.address);
+      await tx.wait();
+      const tagId = await etsToken.computeTagId(taskArgs.tag);
+      if (await etsToken["tagExists(uint256)"](tagId)) {
+        console.log("Tag minted with ID: ", tagId.toString());
+      }
     }
   });
--- packages/contracts-core/tasks/tagTarget.js
+++ packages/contracts-core/tasks/tagTarget.js
@@ -1,46 +1,92 @@
-const {expect} = require("chai");
-
 task("tagTarget", 'Tag a uri. eg: hh tagTarget --tags "#another,#blah" --uri "https://google.com" --network localhost')
   .addParam("uri", 'URI being tagged eg. --uri "https://google.com"')
   .addParam("tags", 'Hashtags separated by commas. eg. --tags "#another,#blah"')
   .addParam("recordType", 'Arbitrary record type identifier. eg. "bookmark"', "bookmark")
   .setAction(async (taskArgs) => {
-    const {setup} = require("../test/setup.js");
+    const {getAccounts} = require("../test/setup.js");
+    const chainId = hre.network.config.chainId;
+    const accounts = await getAccounts();
     const config = require("../config/config.json");
-    const etsAbi = require("../abi/contracts/ETS.sol/ETS.json");
-    const etsTargetTaggerAbi = require("../abi/contracts/targetTaggers/ETSTargetTagger.sol/ETSTargetTagger.json");
 
-    [accounts, contracts, initSettings] = await setup();
-    const etsTargetTaggerAddress = config[31337].contracts.ETSTargetTagger.address;
-    const etsTargetTagger = new ethers.Contract(etsTargetTaggerAddress, etsTargetTaggerAbi, accounts.ETSPlatform);
+    // ABIs
+    const ETSAccessControlsABI = require("../abi/contracts/ETSAccessControls.sol/ETSAccessControls.json");
+    const ETSTargetTaggerABI = require("../abi/contracts/targetTaggers/ETSTargetTagger.sol/ETSTargetTagger.json");
+    const ETSTargetABI = require("../abi/contracts/ETSTarget.sol/ETSTarget.json");
+    const ETSABI = require("../abi/contracts/ETS.sol/ETS.json");
 
-    const etsAddress = config[31337].contracts.ETS.address;
-    const ets = new ethers.Contract(etsAddress, etsAbi, accounts.ETSPlatform);
+    // Contract addresses
+    const ETSAccessControlsAddress = config[chainId].contracts.ETSAccessControls.address;
+    const ETSTargetTaggerAddress = config[chainId].contracts.ETSTargetTagger.address;
+    const ETSTargetAddress = config[chainId].contracts.ETSTarget.address;
+    const ETSAddress = config[chainId].contracts.ETS.address;
 
-    console.log(taskArgs.uri);
-    console.log(taskArgs.tags);
+    // Contract instances
+    const etsAccessControls = new ethers.Contract(ETSAccessControlsAddress, ETSAccessControlsABI, accounts.ETSPlatform);
+    const etsTargetTagger = new ethers.Contract(ETSTargetTaggerAddress, ETSTargetTaggerABI, accounts.ETSPlatform);
+    const etsTarget = new ethers.Contract(ETSTargetAddress, ETSTargetABI, accounts.ETSPlatform);
+    const ets = new ethers.Contract(ETSAddress, ETSABI, accounts.ETSPlatform);
 
-    const tags = taskArgs.tags.replace(/\s+/g, "").split(",");
-
-    let taggingFee = await ets.taggingFee();
-    taggingFee = taggingFee.toString();
-    console.log("taggingFee", taggingFee);
-
-    const tagParams = {
-      targetURI: taskArgs.uri,
-      tagStrings: tags,
-      recordType: taskArgs.recordType,
-      enrich: false,
-    };
-    const taggingRecords = [tagParams];
-
+    // Check that caller is publisher.
     const targetTaggerName = await etsTargetTagger.getTaggerName();
     console.log(targetTaggerName);
+    if (
+      (await etsAccessControls.isPublisher(ETSTargetTaggerAddress)) === false &&
+      (await etsAccessControls["isTargetTagger"](targetTaggerName)) === true
+    ) {
+      console.log(`${etsTargetTagger.address} is not a publisher`);
+      return;
+    }
 
+    const tags = taskArgs.tags.replace(/\s+/g, "").split(","); // remove spaces & split on comma
+    const targetURI = taskArgs.uri;
+    const recordType = taskArgs.recordType;
+
+    let taggingFee = await ets.taggingFee();
+    //taggingFee = taggingFee.toString();
+    //console.log("taggingFee", taggingFee);
+
+    const tagParams = {
+      "targetURI": targetURI,
+      "tagStrings": tags,
+      "recordType": recordType,
+      "enrich": false,
+    };
+    const taggingRecords = [tagParams];
     const tx = await etsTargetTagger.tagTarget(taggingRecords, {
       value: ethers.BigNumber.from(taggingFee).mul(tags.length),
-      gasPrice: ethers.utils.parseUnits("100", "gwei"),
-      gasLimit: 500000,
-      nonce: nonce || undefined,
+      gasPrice: ethers.utils.parseUnits("10", "gwei"),
+      gasLimit: 5000000,
     });
+
+    console.log(`started txn ${tx.hash.toString()}`);
+    tx.wait();
+
+    const targetId = await etsTarget.computeTargetId(targetURI);
+    console.log("target id", targetId.toString());
+
+    // What the heck is going on here? targetExists not found.
+    //if ((await etsTarget["targetExists"](targetURI)) === false) {
+    //  console.log(`${targetId} for ${targetURI} not found`);
+    //  return;
+    //}
+
+    const taggingRecordId = await ets.computeTaggingRecordId(
+      targetId,
+      taskArgs.recordType,
+      ETSTargetTaggerAddress,
+      accounts.ETSPlatform.address,
+    );
+
+    console.log("taggingRecordId", taggingRecordId.toString());
+
+    //    const taggingRecord = await ets.getTaggingRecordFromId(taggingRecordId);
+    //    console.log(taggingRecord);
+    //
+    //    console.log("taggingRecord.targetId", taggingRecord.targetId.toString());
+    //    console.log("targetId", targetId.toString());
+    //
+    //    if (taggingRecord.targetId.toString() !== targetId.toString()) {
+    //      console.log(`tagging record not found for ${taggingRecordId}`);
+    //    }
+    //    console.log(`tagging record created or edited ${taggingRecordId}`);
   });
