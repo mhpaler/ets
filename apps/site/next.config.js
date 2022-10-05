ceef783dc760357750e7dcc094908e96e956dae5
swaylock <michael@ets.xyz>
2022-10-05 15:09:40 -0700

    Homepage.
    gh-212

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,27 +1,7 @@
-const withTM = require("next-transpile-modules")([
-  "@ethereum-tag-service/ui"
-]);
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
-  unstable_staticImage: true
-});
-
-/**
- * @type {import('next').NextConfig}
- **/
- const nextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/explorer',
-        destination: 'https://stage.app.ets.xyz/',
-        permanent: false,
-        basePath: false
-      },
-    ]
-  },
-};
+  swcMinify: true,
+}
 
-module.exports = withNextra(withTM(nextConfig));
+module.exports = nextConfig
