7f7d4e54ceabe7cb63058083090b5affceadd4b0
swaylock <michael@ets.xyz>
2022-10-05 16:05:23 -0700

    Change link to substack.
    gh-212

--- apps/site/pages/index.js
+++ apps/site/pages/index.js
@@ -63,7 +63,7 @@ export default function Home() {
                 </svg>
               </a>
 
-              <a href="https://hashtagprotocol.substack.com/" className="text-slate-600 hover:text-slate-500" target="_blank">
+              <a href="https://etsxyz.substack.com/" className="text-slate-600 hover:text-slate-500" target="_blank">
                 <span className="sr-only"></span>
                 <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
                   <path d="M22.539 8.242H1.46V5.406h21.08v2.836zM1.46 10.812V24L12 18.11 22.54 24V10.812H1.46zM22.54 0H1.46v2.836h21.08V0z" />
