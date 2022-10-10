5cb1aeed29cb8ed2e13906b7218d3f742024232d
swaylock <michael@ets.xyz>
2022-10-10 16:46:29 -0700

    Fix build error.
    gh-201

--- apps/app/components/RecentlyTagged.tsx
+++ apps/app/components/RecentlyTagged.tsx
@@ -7,26 +7,6 @@ import { useTaggingRecords } from "../hooks/useTaggingRecords";
 import { Table } from "../components/Table";
 
 const pageSize = 5;
-
-const go = (
-  <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
-    <path
-      stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="1.5"
-      d="M13.75 6.75L19.25 12L13.75 17.25"
-    ></path>
-    <path
-      stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="1.5"
-      d="M19 12H4.75"
-    ></path>
-  </svg>
-);
-
 const RecentlyTagged = () => {
   const { t } = useTranslation("common");
   const { taggingRecords } = useTaggingRecords({ pageSize: pageSize });
