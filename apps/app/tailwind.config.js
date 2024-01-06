aa6475fc7b6a5082b014a9dcbd607674a92409a4
mpaler <mpaler@gmail.com>
2024-01-06 14:39:37 -0800

    Disable SIWE and network selector.
    gh-246

--- apps/app/tailwind.config.js
+++ apps/app/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [
-    require("tailwindcss-font-inter"),
+    //require("tailwindcss-font-inter"),
     require("daisyui"),
     require('@tailwindcss/forms'),
   ],
