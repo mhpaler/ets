195a49bac94d6f1745a10b650fb7fc8b3cdb262e
mpaler <mpaler@gmail.com>
2024-01-17 11:37:57 -0800

    Cleanup default app template.
    gh-264

--- apps/app/layouts/default.tsx
+++ apps/app/layouts/default.tsx
@@ -1,8 +1,5 @@
 import React, { ReactNode } from "react";
-//import Head from "next/head";
-import DrawerToggle from "@app/components/DrawerToggle";
 import Navigation from "@app/components/Navigation";
-import type { Metadata } from "next";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
@@ -33,43 +30,6 @@ export default function Layout({ children }: { children: ReactNode }) {
         </div>
       </div>
       <Footer />
-
-      {/*       <div className="drawer lg:drawer-open">
-        <input id="main-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col p-4 lg:p-10">
-          <Header />
-          <main>{children}</main>
-          <label htmlFor="main-drawer" className="btn btn-primary drawer-button lg:hidden">
-            Open drawer
-          </label>
-        </div>
-        <div className="drawer-side">
-          <label htmlFor="main-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-          <Navigation />
-        </div>
-      </div>
-      <Footer /> */}
-
-      {/* <div className="drawer lg:drawer-open">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col items-center justify-center">
-          {children}
-          <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
-            Open drawer
-          </label>
-        </div>
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
-          </ul>
-        </div>
-      </div> */}
     </>
   );
 }
