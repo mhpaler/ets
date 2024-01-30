b7d475993c3288836aca2505221e90a4929cf89d
mpaler <mpaler@gmail.com>
2024-01-30 08:43:51 -0800

    Fix .env typo.
    gh-254

--- apps/app/constants/wagmiConfig.ts
+++ apps/app/constants/wagmiConfig.ts
@@ -1,20 +1,17 @@
-import { http, createConfig, Config, fallback } from 'wagmi'
-import { polygonMumbai, hardhat } from 'wagmi/chains'
-import { lightTheme, Theme } from '@rainbow-me/rainbowkit'
-import { injected } from 'wagmi/connectors'
+import { http, createConfig, Config, fallback } from "wagmi";
+import { polygonMumbai, hardhat } from "wagmi/chains";
+import { lightTheme, Theme } from "@rainbow-me/rainbowkit";
+import { injected } from "wagmi/connectors";
 import merge from "lodash.merge";
 
-
 export const wagmiConfig: Config = createConfig({
-  chains: [process.env.NEXT_PUBLIC_ETS_ENVIRONMEENT === 'development' ? hardhat : polygonMumbai],
+  chains: [process.env.NEXT_PUBLIC_ETS_ENVIRONMENT === "development" ? hardhat : polygonMumbai],
   connectors: [injected()],
   transports: {
-    [polygonMumbai.id]: fallback([
-      http('https://polygon-mumbai-pokt.nodies.app/'),
-    ]),
-    [hardhat.id]: http('http://localhost:8545'),
+    [polygonMumbai.id]: fallback([http("https://polygon-mumbai-pokt.nodies.app/")]),
+    [hardhat.id]: http("http://localhost:8545"),
   },
-})
+});
 
 export const etsTheme = merge(
   lightTheme({
