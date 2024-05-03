5b900c0e7b9205f07025b3988e2132aca4f013ce
vidvidvid <weetopol@gmail.com>
2024-05-03 15:14:02 +0200

    add token client methods

--- packages/sdk-core/src/TokenClient.ts
+++ packages/sdk-core/src/TokenClient.ts
@@ -1,4 +1,4 @@
-import type { Account, Hex, PublicClient, WalletClient } from "viem";
+import type { PublicClient, WalletClient, Hex } from "viem";
 import { etsTokenConfig } from "../contracts/contracts";
 
 export class TokenClient {
@@ -19,78 +19,24 @@ export class TokenClient {
     this.publicClient = publicClient;
     this.walletClient = walletClient;
 
-    if (publicClient === undefined) {
+    if (!publicClient) {
       throw new Error("Public client is required");
     }
-
     if (publicClient.chain?.id !== chainId) {
       throw new Error("Provided chain id should match the public client chain id");
     }
-
-    if (walletClient !== undefined && walletClient.chain?.id !== chainId) {
+    if (walletClient && walletClient.chain?.id !== chainId) {
       throw new Error("Provided chain id should match the wallet client chain id");
     }
   }
 
-  async tagExists(tag: string): Promise<boolean> {
-    const tagId = await this.computeTagId(tag);
-
-    try {
-      const exists = await this.publicClient.readContract({
-        ...etsTokenConfig,
-        functionName: "tagExistsById",
-        args: [tagId],
-      });
-
-      return exists;
-    } catch (error) {
-      console.error("Error checking if tag exists:", error);
-      throw error;
-    }
-  }
-
   async existingTags(tags: string[]): Promise<string[]> {
-    try {
-      const existingTags = [];
-
-      for (let tag of tags) {
-        const exists = await this.tagExists(tag);
-
-        if (exists) {
-          existingTags.push(tag);
-        }
-      }
-
-      return existingTags;
-    } catch (error) {
-      console.error("Error checking if tag exists:", error);
-      throw error;
+    const existingTags = [];
+    for (let tag of tags) {
+      const exists = await this.tagExists(tag);
+      if (exists) existingTags.push(tag);
     }
-  }
-
-  async computeTagId(tag: string): Promise<bigint> {
-    try {
-      const tagId = await this.publicClient.readContract({
-        ...etsTokenConfig,
-        functionName: "computeTagId",
-        args: [tag],
-      });
-
-      return tagId;
-    } catch (error) {
-      console.error("Error computing tag ID:", error);
-      throw error;
-    }
-  }
-
-  async computeTagIds(tags: string[]): Promise<bigint[]> {
-    try {
-      const tagIds = await Promise.all(tags.map((tag) => this.computeTagId(tag)));
-      return tagIds;
-    } catch (error) {
-      console.error("Error computing tag IDs:", error);
-      throw error;
-    }
+    return existingTags;
   }
 
   async fetchHasTags(address: `0x${string}` | undefined): Promise<boolean> {
@@ -98,12 +44,117 @@ export class TokenClient {
       console.error("Invalid address");
       return false;
     }
-    const data = await this.publicClient.readContract({
-      ...etsTokenConfig,
-      functionName: "balanceOf",
-      args: [address],
-    });
-
+    const data = await this.manageContractRead("balanceOf", [address]);
     return data > BigInt(0);
+  }
+
+  async tagExists(tag: string): Promise<boolean> {
+    const tagId = await this.computeTagId(tag);
+    return this.manageContractRead("tagExistsById", [tagId]);
+  }
+
+  async computeTagId(tag: string): Promise<bigint> {
+    return this.manageContractRead("computeTagId", [tag]);
+  }
+
+  async computeTagIds(tags: string[]): Promise<bigint[]> {
+    return Promise.all(tags.map((tag) => this.computeTagId(tag)));
+  }
+
+  async approve(to: Hex, tokenId: bigint): Promise<{ transactionHash: string; status: number }> {
+    return this.manageContractCall("approve", [to, tokenId]);
+  }
+
+  async balanceOf(owner: Hex): Promise<bigint> {
+    return this.manageContractRead("balanceOf", [owner]);
+  }
+
+  async burn(tokenId: bigint): Promise<{ transactionHash: string; status: number }> {
+    return this.manageContractCall("burn", [tokenId]);
+  }
+
+  async createTag(tag: string, relayer: Hex, creator: Hex): Promise<bigint> {
+    const { transactionHash, status } = await this.manageContractCall("createTag", [tag, relayer, creator]);
+    return this.manageContractRead("getOrCreateTagId", [tag, relayer, creator]);
+  }
+
+  async getApproved(tokenId: bigint): Promise<Hex> {
+    return this.manageContractRead("getApproved", [tokenId]);
+  }
+
+  async getTagById(tokenId: bigint): Promise<any> {
+    return this.manageContractRead("getTagById", [tokenId]);
+  }
+
+  async getTagByString(tag: string): Promise<any> {
+    return this.manageContractRead("getTagByString", [tag]);
+  }
+
+  async isApprovedForAll(owner: Hex, operator: Hex): Promise<boolean> {
+    return this.manageContractRead("isApprovedForAll", [owner, operator]);
+  }
+
+  async ownerOf(tokenId: bigint): Promise<Hex> {
+    return this.manageContractRead("ownerOf", [tokenId]);
+  }
+
+  async setApprovalForAll(operator: Hex, approved: boolean): Promise<{ transactionHash: string; status: number }> {
+    return this.manageContractCall("setApprovalForAll", [operator, approved]);
+  }
+
+  async transferFrom(from: Hex, to: Hex, tokenId: bigint): Promise<{ transactionHash: string; status: number }> {
+    return this.manageContractCall("transferFrom", [from, to, tokenId]);
+  }
+
+  async pause(): Promise<{ transactionHash: string; status: number }> {
+    return this.manageContractCall("pause", []);
+  }
+
+  async unPause(): Promise<{ transactionHash: string; status: number }> {
+    return this.manageContractCall("unPause", []);
+  }
+
+  // Helper methods for contract interactions
+  // fix any
+  private async manageContractRead(functionName: any, args: any = []): Promise<any> {
+    try {
+      return await this.publicClient.readContract({
+        ...etsTokenConfig,
+        functionName,
+        args,
+      });
+    } catch (error) {
+      console.error(`Error performing read operation for function ${functionName}:`, error);
+      throw error;
+    }
+  }
+
+  // fix any
+  private async manageContractCall(
+    functionName: any,
+    args: any = [],
+  ): Promise<{ transactionHash: string; status: number }> {
+    if (!this.walletClient) {
+      throw new Error("Wallet client is required to perform this action");
+    }
+    try {
+      const { request } = await this.publicClient.simulateContract({
+        address: etsTokenConfig.address,
+        abi: etsTokenConfig.abi,
+        functionName,
+        args,
+        account: this.walletClient.account,
+      });
+
+      const hash = await this.walletClient.writeContract(request);
+      const receipt = await this.publicClient.waitForTransactionReceipt({ hash });
+      return {
+        status: receipt.status,
+        transactionHash: receipt.transactionHash,
+      };
+    } catch (error) {
+      console.error(`Error in ${functionName}:`, error);
+      throw error;
+    }
   }
 }
