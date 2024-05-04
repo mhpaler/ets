bcf05dd24be65759210e0b5a751ed10e7c80e4dd
vidvidvid <weetopol@gmail.com>
2024-05-04 18:58:05 +0200

    export new clients

--- packages/sdk-core/src/index.ts
+++ packages/sdk-core/src/index.ts
@@ -1,5 +1,7 @@
 import { AccessControlsClient } from "./AccessControlsClient";
 import { AuctionHouseClient } from "./AuctionHouseClient";
+import { CoreClient } from "./CoreClient";
+import { EnrichTargetClient } from "./EnrichTargetClient";
 import { ETSClient } from "./ETSClient";
 import { RelayerClient } from "./RelayerClient";
 import { RelayerFactoryClient } from "./RelayerFactoryClient";
@@ -9,6 +11,8 @@ import { TokenClient } from "./TokenClient";
 export {
   AccessControlsClient,
   AuctionHouseClient,
+  CoreClient,
+  EnrichTargetClient,
   ETSClient,
   RelayerClient,
   RelayerFactoryClient,
