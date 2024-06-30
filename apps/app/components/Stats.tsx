f3fd56f7998d18a0c1dd54470e3c3d040c32bdf9
mpaler <mpaler@gmail.com>
2024-06-30 15:22:47 -0700

    Clean up left menu.
    gh-305

--- apps/app/components/Stats.tsx
+++ apps/app/components/Stats.tsx
@@ -16,33 +16,29 @@ const Stats = () => {
   const { t } = useTranslation("common");
 
   return (
-    <section className="stats stats-vertical col-span-12 w-full shadow-sm xl:stats-horizontal">
-      <a href="/tagging-records" className="hover:bg-slate-300">
+    <section className="stats stats-vertical col-span-12 w-full border shadow-sm xl:stats-horizontal">
+      <a href="/tagging-records" className="hover:bg-base-200">
         <div className="stat">
           <div className="stat-title">{t("tagging-records")}</div>
           <div className="stat-value">{stats && stats.taggingRecordsCount ? stats.taggingRecordsCount : null}</div>
-          <div className="stat-desc">21% more than last month</div>
         </div>
       </a>
-      <a href="/tags" className="hover:bg-slate-300">
+      <a href="/tags" className="hover:bg-base-200">
         <div className="stat">
           <div className="stat-title">{t("tags")}</div>
           <div className="stat-value">{stats && stats.tagsCount ? stats.tagsCount : null}</div>
-          <div className="stat-desc">21% more than last month</div>
         </div>
       </a>
-      <a href="/relayers" className="hover:bg-slate-300">
+      <a href="/relayers" className="hover:bg-base-200">
         <div className="stat">
           <div className="stat-title">{t("relayers")}</div>
           <div className="stat-value">{stats && stats.relayerCountActive ? stats.relayerCountActive : null}</div>
-          <div className="stat-desc">21% more than last month</div>
         </div>
       </a>
-      <a href="/tagging-records" className="hover:bg-slate-300">
+      <a href="/tagging-records" className="hover:bg-base-200">
         <div className="stat">
           <div className="stat-title">{t("taggers")}</div>
           <div className="stat-value">{stats && stats.taggerCount ? stats.taggerCount : null}</div>
-          <div className="stat-desc">21% more than last month</div>
         </div>
       </a>
     </section>
