666b008c2318739a9c026f90e826b54ae4c4acce
vidvidvid <weetopol@gmail.com>
2024-07-28 19:08:43 +0200

    add rainbowkit provider

--- examples/create-tagging-record/src/pages/_app.jsx
+++ examples/create-tagging-record/src/pages/_app.jsx
@@ -1,7 +1,10 @@
 import { WagmiProvider } from "wagmi";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { wagmiConfig, queryClient } from "../config";
 import { useEffect, useState } from "react";
+
+import "@rainbow-me/rainbowkit/styles.css";
 
 function MyApp({ Component, pageProps }) {
   const [mounted, setMounted] = useState(false);
@@ -17,7 +20,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <RainbowKitProvider chains={wagmiConfig.chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
