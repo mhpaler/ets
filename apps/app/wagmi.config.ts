fc2f211d595ab223d551327e1e79bbe18cbe12ae
mpaler <mpaler@gmail.com>
2024-07-30 20:21:07 -0700

    Build debugging.
    gh-330

--- apps/app/wagmi.config.ts
+++ apps/app/wagmi.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "@wagmi/cli";
 import hardhatDeploy from "@sunodo/wagmi-plugin-hardhat-deploy";
+import dotenv from "dotenv";
 
-const network = process.env.NETWORK ? process.env.NETWORK : "localhost";
-console.log("process.env.NETWORK", process.env.NETWORK);
+// Optionally load .env file if running locally
+dotenv.config({ path: "../../.env" });
+
+console.log("### Wagmi Config ###");
+console.log("All Environment Variables:", process.env);
+console.log("Specific Environment Variable NETWORK:", process.env.NETWORK);
+
+const network = process.env.NETWORK || "localhost";
+console.log("Resolved network:", network);
 
 export default defineConfig({
   out: "src/contracts.ts",
