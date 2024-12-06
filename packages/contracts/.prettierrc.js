c2ca7e9d508a8373206af59c9b125f3e7574bf46
mpaler <mpaler@gmail.com>
2024-12-06 15:11:19 -0800

    Update solhint depenencies, fix command.
    gh-459

--- packages/contracts/.prettierrc.js
+++ packages/contracts/.prettierrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-  plugins: [require("prettier-plugin-solidity")],
+  plugins: ["prettier-plugin-solidity"],
   overrides: [
     {
       files: "*.sol",
       options: {
+        parser: "solidity-parse",
         printWidth: 120,
         tabWidth: 4,
         useTabs: false,
         singleQuote: false,
-        semi: false,
         bracketSpacing: true,
         explicitTypes: "always",
+        semi: false,
       },
     },
     {
