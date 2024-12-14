6060efef305f2a65f74860b23f322aa6278720f0
mpaler <mpaler@gmail.com>
2024-12-14 08:11:14 -0800

    Lint site.
    gh-320

--- apps/site/snippets/walletClient.ts
+++ apps/site/snippets/walletClient.ts
@@ -1,12 +1,12 @@
-import 'viem/window'
+import "viem/window";
 
 // ---cut---
 // [!region imports]
-import { createWalletClient, custom } from 'viem'
-import { mainnet } from 'viem/chains'
+import { createWalletClient, custom } from "viem";
+import { mainnet } from "viem/chains";
 // [!endregion imports]
 
 export const walletClient = createWalletClient({
   chain: mainnet,
   transport: custom(window.ethereum!),
-})
+});
